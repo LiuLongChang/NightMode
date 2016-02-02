69953165738634c7a0571888638cc0e5b78e372c
LiuLongChang <1040719208@qq.com>
2016-02-02 13:54:36 +0800

    Initial Commit

---
+++ NightMode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NightMode
+//
+//  Created by langyue on 16/2/2.
+//  Copyright © 2016年 langyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
